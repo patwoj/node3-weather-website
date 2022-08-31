0233f64815cd76297c186ac433dcacb90b45f960
patwoj <patwoj@gmail.com>
2022-08-31 23:35:22 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=dea8df8595338beb73c4b42d638e47eb&query=' + latitude + ',' + longitude + '&units=m'
+    request({url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out.')
+        }  
+    })
+}
+
+module.exports = forecast
